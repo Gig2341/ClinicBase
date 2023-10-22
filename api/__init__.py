e4d6fe98d5917062f9bdd4c565edaa43ce2ec694
Dtikoli <tikolidavid@yahoo.com>
2023-10-22 12:31:04 -0700

    Updating initial files

--- api/__init__.py
+++ api/__init__.py
@@ -3,11 +3,11 @@
 from flask import Blueprint, make_response, jsonify
 from flask_cors import CORS
 
-bp_api = Blueprint('api', __name__)
+bp_api = Blueprint('api', __name__, url_prefix='/api')
 cors = CORS(bp_api, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@bp_app.errorhandler(404)
+@bp_api.errorhandler(404)
 def not_found(error):
     """ 404 Error """
     return make_response(jsonify({'error': "Not found"}), 404)
