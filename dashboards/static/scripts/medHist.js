05dd7e78e801336387362fba2d00c69bdcb0b4fb
Dtikoli <tikolidavid@yahoo.com>
2023-11-09 11:20:39 -0800

    Reviewing patient search option

--- dashboards/static/scripts/medHist.js
+++ dashboards/static/scripts/medHist.js
@@ -1,69 +1,61 @@
-function medHistoryAPICalls () {
+let patientId = null;
+
+function getMedicalRecords () {
+  if (patientId) {
+    fetch(`https://clinicbase.tech/api/medical_records/${patientId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        generateAndPreviewHtml(data);
+        document.getElementById('radio-button-list').innerHTML = '';
+      });
+  }
+}
+
+function handleSelection () {
+  const selectedRadio = document.querySelector('input[name="patient"]:checked');
+  if (selectedRadio) {
+    patientId = selectedRadio.value;
+  }
+}
+
+function handleSearchButtonClick () {
   const searchText = document.getElementById('searchText').value;
   const searchFilter = document.getElementById('searchFilter').value;
   const radioList = document.getElementById('radio-button-list');
-  let patientId = null;
 
   const requestData = {
     [searchFilter]: searchText
   };
 
-  const getMedicalRecords = (patientId) => {
-    fetch(`https://clinicbase.tech/api/medical_records/${patientId}`)
-      .then(response => response.json())
-      .then(data => {
-        generateAndPreviewHtml(data);
-        radioList.innerHTML = '';
+  fetch('https://clinicbase.tech/api/patients/search', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      radioList.innerHTML = '';
+
+      document.getElementById('searchPatientForm').reset();
+      data.forEach((patient) => {
+        const listItem = document.createElement('li');
+        const radio = document.createElement('input');
+        radio.type = 'radio';
+        radio.name = 'patient';
+        radio.id = `patient${patient.id}`;
+        radio.value = patient.id;
+        const label = document.createElement('label');
+        label.htmlFor = `patient${patient.id}`;
+        label.textContent = `Name: ${patient.firstname} ${patient.surname}, DOB: ${patient.dob}`;
+        listItem.appendChild(radio);
+        listItem.appendChild(label);
+        radioList.appendChild(listItem);
       });
-  };
 
-  const handleSelection = () => {
-    const selectedRadio = document.querySelector('input[name="patient"]:checked');
-    if (selectedRadio) {
-      patientId = selectedRadio.value;
-    }
-  };
-
-  const handleSearchButtonClick = () => {
-    fetch('https://clinicbase.tech/api/patients/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(requestData)
-    })
-      .then(response => response.json())
-      .then(data => {
-        radioList.innerHTML = '';
-
-        document.getElementById('searchPatientForm').reset();
-        data.forEach(patient => {
-          const listItem = document.createElement('li');
-          const radio = document.createElement('input');
-          radio.type = 'radio';
-          radio.name = 'patient';
-          radio.id = `patient${patient.id}`;
-          radio.value = patient.id;
-          const label = document.createElement('label');
-          label.htmlFor = `patient${patient.id}`;
-          label.textContent = `Name: ${patient.firstname} ${patient.surname}, DOB: ${patient.dob}`;
-          listItem.appendChild(radio);
-          listItem.appendChild(label);
-          radioList.appendChild(listItem);
-        });
-        radioList.addEventListener('click', handleSelection);
-      });
-  };
-
-  document.getElementById('searchPatientForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    handleSearchButtonClick();
-  });
-  document.getElementById('patientRecordButton').addEventListener('click', () => {
-    if (patientId) {
-      getMedicalRecords(patientId);
-    }
-  });
+      radioList.addEventListener('click', handleSelection);
+    });
 }
 
 function generateAndPreviewHtml (responseData) {
@@ -96,7 +88,13 @@ function generateAndPreviewHtml (responseData) {
     }
   }
 
+  container.innerHTML = '';
   generateHtmlContent(responseData);
 }
 
-medHistoryAPICalls();
+document.getElementById('searchPatientForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  handleSearchButtonClick();
+});
+
+document.getElementById('patientRecordButton').addEventListener('click', getMedicalRecords);
