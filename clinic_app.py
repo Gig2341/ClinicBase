b25acc7837b34b19ee3f5ae89b4b9ad9d40f8ece
Dtikoli <tikolidavid@yahoo.com>
2023-11-21 15:18:05 +0000

    Implementing custom 404 error page

--- clinic_app.py
+++ clinic_app.py
@@ -3,7 +3,7 @@
 
 from models import storage
 from dashboards import create_app
-
+from flask import render_template
 
 app = create_app()
 
@@ -12,6 +12,11 @@ app = create_app()
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('custom_404.html'), 404
 
 
 if __name__ == '__main__':
